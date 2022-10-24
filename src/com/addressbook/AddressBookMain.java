508f82d9d0727b81870eead654a3790bf2a02327
hhmadan <hhmadan03@gmail.com>
2022-10-24 23:53:55 +0530

    Address Book Code

---
+++ src/com/addressbook/AddressBookMain.java
@@ -0,0 +1,47 @@
+package com.addressbook;
+
+import java.util.Scanner;
+
+public class AddressBookMain {
+        static final Scanner scanner = new Scanner(System.in);
+
+        public static void main(String[] args) throws Exception
+        {
+            Book addressBookMain = new Book();
+            while (true)
+            {
+                System.out.println("-----------------------------");
+                System.out.println("1.Create a new Contact");
+                System.out.println("2. Enter Available Contact Details");
+                System.out.println("3. Edit Contact");
+                System.out.println("4.Delete Contact");
+                System.out.println("5.Exit");
+                System.out.print("Enter Option: ");
+                int option = scanner.nextInt();
+                switch (option)
+                {
+                    case 1:
+                        addressBookMain.createNewContact();
+                        break;
+                    case 2:
+                        addressBookMain.fillContactDetails();
+                        break;
+                    case 3:
+                        addressBookMain.editContact();
+                        break;
+                    case 4:
+                        addressBookMain.deleteContact();
+                        break;
+                    case 5:
+                        System.out.println("exiting address book....");
+                        System.exit(0);
+                        break;
+                    default:
+                        System.out.println("Error..! Please Enter valid Choice");
+                        break;
+                }
+
+            }
+
+        }
+}
---
+++ src/com/addressbook/Book.java
@@ -0,0 +1,156 @@
+package com.addressbook;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Book {
+    static final Scanner sc = new Scanner(System.in);
+    static ArrayList<String> voidContacts = new ArrayList<>();
+    static ArrayList<String> nonVoidContacts = new ArrayList<>();
+
+    void createNewContact() throws Exception
+    {
+        System.out.print("Enter New Contact Name: ");
+        String contactName = sc.nextLine();
+        File file = new File(contactName);
+        if (file.exists())
+        {
+            System.out.println(file.getName() + " Already exists..!");
+        }
+        else
+        {
+            if (file.createNewFile())
+            {
+                System.out.println("New Contact " + file.getName() + " is created Successfully..!");
+                voidContacts.add(file.getName());
+            }
+        }
+
+    }
+
+    void fillContactDetails() throws Exception
+    {
+        System.out.print("Enter Contact Name having details missing:");
+        String contactName = sc.nextLine();
+        if (voidContacts.contains(contactName))
+        {
+
+            FileWriter fw = new FileWriter(contactName);
+            String details = "";
+            System.out.print("Enter first name:");
+            details += sc.nextLine() + "\n";
+            System.out.print("Enter last name:");
+            details += sc.nextLine() + "\n";
+            System.out.print("Enter address:");
+            details += sc.nextLine() + "\n";
+            System.out.print("Enter city:");
+            details += sc.nextLine() + "\n";
+            System.out.print("Enter state:");
+            details += sc.nextLine() + "\n";
+            System.out.print("Enter zip:");
+            details += sc.nextLine() + "\n";
+            System.out.print("Enter mobile number:");
+            details += sc.nextLine() + "\n";
+            fw.write(details);
+            voidContacts.remove(contactName);
+            nonVoidContacts.add(contactName);
+            System.out.println("Contact Saved Successfully..!");
+            fw.close();
+        }
+        else
+        {
+            System.out.println(contactName + " Already exists or Not created..!");
+        }
+
+    }
+
+    void editContact()throws Exception
+    {
+        System.out.print("Enter Name of the contact to edit:");
+        String contactName = sc.nextLine();
+        if(voidContacts.contains(contactName))
+        {
+            System.out.println("Please fill the details of "+ contactName + " before editing it");
+            return;
+        }
+        else if(!nonVoidContacts.contains(contactName))
+        {
+            System.out.println("Please create the contact "+ contactName +" before editing it");
+            return;
+        }
+        System.out.println("\nThe details of " + contactName + " at present is:");
+        readFile(contactName);
+        System.out.println("\nEnter details to edit...\nFirst Name:\nLast Name:\nAddress:\nCity:\nState:\nZip:\nMobile Number: ");
+        String newContent="";
+        for (int i = 1; i <= 7; i++)
+        {
+            newContent += sc.nextLine() + "\n";
+        }
+        String option;
+        do
+        {
+            System.out.println("Press\nS for SAVE        C for CANCEL");
+            option = sc.nextLine();
+        } while (!(option.equalsIgnoreCase("s")  || option.equalsIgnoreCase("c")));
+        switch(option)
+        {
+            case "s":
+            case "S":
+                writeFile(contactName, newContent);
+                System.out.println(contactName + " is saved with new content");
+                break;
+            case "c":
+            case "C":
+                System.out.println("changes are not saved");
+                return;
+            default:
+                System.out.println("please select either S or C");
+        }
+
+    }
+    void readFile(String fileName)throws Exception
+    {
+        FileReader fileReader = new FileReader(fileName);
+        int character;
+        while ((character = fileReader.read()) != -1)
+        {
+            System.out.print((char) character);
+        }
+        fileReader.close();
+    }
+    void writeFile(String fileName,String content)throws Exception
+    {
+        FileWriter fw = new FileWriter(fileName);
+        fw.write(content);
+        fw.close();
+    }
+
+    void deleteContact()
+    {
+        System.out.print("Enter contact name to delete:");
+        String contactName = sc.nextLine().trim();
+        File file = new File(contactName);
+        if(file.exists())
+        {
+            if(file.delete())
+            {
+                if(voidContacts.contains(contactName))
+                {
+                    voidContacts.remove(contactName);
+                }
+                else
+                {
+                    nonVoidContacts.remove(contactName);
+                }
+                System.out.println(contactName + " is deleted Successfully..!");
+            }
+        }
+        else
+        {
+            System.out.println(contactName + " doesn't exists");
+        }
+    }
+}
